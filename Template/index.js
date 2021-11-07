00458c98be798eb7d47eac8c9bab3b480e2abb9c
Luca <93574921+zuverliebt@users.noreply.github.com>
2021-11-07 18:52:32 +0100

    Update index.js

--- Template/index.js
+++ Template/index.js
@@ -17,7 +17,7 @@ client.on('interactionCreate', async interaction => {
     const command = client.commands.get(interaction.commandName);
     if (!command) return;
     try {
-        await command.execute(interaction);
+        await command.execute(client, interaction);
     } catch (error) {
         console.error(error);
         return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
